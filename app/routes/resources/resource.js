34fd77435fdbfb0b97feab54bf68adc054a39b10
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-10-29 15:49:03 -0400

    toggle editing

--- app/routes/resources/resource.js
+++ app/routes/resources/resource.js
@@ -1,16 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
+  model(params) {
     return this.store.find('resource', params.resource_id);
   },
   actions: {
-    delete: function() {
+    delete() {
       var resource = this.currentModel;
       resource.deleteRecord();
       resource.save().then(() => {
         this.transitionTo('resources');
       });
-    }
+    },    
   }
 });
+
