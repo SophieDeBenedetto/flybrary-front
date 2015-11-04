724d13836d9d3b982a20a7ba7759c254fc5b0760
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-11-04 11:41:13 -0500

    upgraded to newest ember version, templates rendering and edit toggling working

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,16 +1,6 @@
 import DS from 'ember-data';
 import ActiveModelAdapter from 'active-model-adapter';
 
-
-// export default DS.RESTAdapter.extend({  
-//   namespace: 'v1',
-//   host: 'http://localhost:4000'
-// });
-
-
-// import DS from 'ember-data';
-// // import ActiveModelAdapter from 'active-model-adapter';
-
 export default ActiveModelAdapter.extend({
   namespace: 'v1',
   host: 'http://localhost:4000'
